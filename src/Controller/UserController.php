112403886305fe4b19d83079f4ca22f5a3014d5b
ThBM <thibault.barolatmassole@gmail.com>
2019-07-05 22:00:53 +0200

    Added: FOSRestBundle

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Rest\Get("/user", name="user")
      */
     public function index()
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $user = new User();
+        $user->setEmail("thibault.barolatmassole@gmail.com");
+
+        $view = new View($user);
+        return $view;
     }
 }
